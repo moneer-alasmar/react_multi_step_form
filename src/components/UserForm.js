7cb5620c84a15445e6ed0d3e0d9676cb2348d7e7
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-07 07:47:48 -0800

    began creating user form

---
+++ src/components/UserForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+  // Proceed to previos step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.targe.value });
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+export default UserForm;
