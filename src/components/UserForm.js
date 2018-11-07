a5bbee54f77cdc1eb471d75efd8978b38552b7bb
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-07 08:28:02 -0800

    completed user details first page

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
 
 export class UserForm extends Component {
   state = {
@@ -28,11 +29,37 @@ export class UserForm extends Component {
 
   // Handle fields change
   handleChange = input => e => {
-    this.setState({ [input]: e.targe.value });
+    this.setState({ [input]: e.target.value });
   };
 
   render() {
-    return <div />;
+    const {
+      step,
+      firstName,
+      lastName,
+      email,
+      occupation,
+      city,
+      bio
+    } = this.state;
+    const values = { step, firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
 
