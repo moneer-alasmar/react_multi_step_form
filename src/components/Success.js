2bf8a1b6a4f98af667e6060f4943bc2b41b14b3e
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-07 08:47:33 -0800

    completed step form

---
+++ src/components/Success.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import { List, ListItem } from "material-ui/List";
+import RaisedButton from "material-ui/RaisedButton";
+
+export class Success extends Component {
+  continue = e => {
+    e.preventDefault();
+    // PROCESS FORM HERE //
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Success" />
+          <h1>Thank You For Your Submission</h1>
+          <p>You will receive a confirmation email to verify your address.</p>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default Success;
